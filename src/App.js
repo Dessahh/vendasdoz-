45dd4ce18b05abfc757a4a7d48ddc582d865e4e0
Wilson Martins <wilson@kissolutions.tech>
2018-10-08 18:29:32 -0300

    Feature: Product list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import logo from './logo2.png'
 import './App.css'
+import 'react-table/react-table.css'
 
 import Cadastro from './Cadastro.js'
 import Consulta from './Consulta.js'
+import Produto from './Produto.js'
 import Error from './Error.js'
 
 class App extends Component {
@@ -15,6 +17,7 @@ class App extends Component {
         <Switch>
           <Route path='/consulta' component={Consulta} />
           <Route path='/cadastro' component={Cadastro} />
+          <Route path='/produto' component={Produto} />
           <Route component={Error} />
         </Switch>
       </BrowserRouter>
