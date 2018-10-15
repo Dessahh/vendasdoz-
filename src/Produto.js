2a7583ac158f33fc8da5965e946f8cc69d2f31ef
Wilson Martins <wilson@kissolutions.tech>
2018-10-15 19:46:02 -0300

    Fix: Apresentacao

--- src/Produto.js
+++ src/Produto.js
@@ -124,14 +124,14 @@ export default class Produto extends React.Component {
             // {Header: 'Name', accessor: 'name', width: 200},
             {Header: 'Description', accessor: 'description', width: 500},
             {Header: 'Type', accessor: 'type', width: 100},
-            {Header: 'Category', accessor: 'category', width: 200},
+            // {Header: 'Category', accessor: 'category', width: 200},
             // {Header: 'Quantity in Stock', accessor: 'quantityInStock', width: 100},
             {Header: 'Value', accessor: 'value', width: 100},
             {Header: 'Manufacturer', accessor: 'manufacturer', width: 200},
             {
                 Header: "Image",
                 Cell: (row) => {
-                    return <div><img height={60} src={row.images ? row.images[0] : null}/></div>
+                    return <div><img height={60} src={row.imageURLs ? row.imageURLs[0] : null}/></div>
                 },
                 accessor: 'images',
                 id: "image"
